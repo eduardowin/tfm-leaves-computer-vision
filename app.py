94d8c65bf2c91a0969ad825380896915c79f2310
Eduardo Zuniga <lord_zeg25@hotmail.com>
2023-09-24 03:35:08 -0500

    Advance development

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-
 from io import BytesIO
 from flask import Flask, Response, jsonify, current_app
 from flask import make_response, send_file
-import time
-
 import json
 import re
 import os
-import sys
 import requests
 
 from datetime import datetime
@@ -15,12 +11,10 @@ from time import sleep
 from threading import Thread
 from functools import wraps
 import os
-import asyncio
 from flask_cors import CORS
 from flask import Flask, render_template, request,jsonify
 
 from math import expm1
-import joblib
 import pandas as pd
 from tensorflow import keras
 
@@ -34,7 +28,7 @@ from tensorflow.keras.models import load_model
 
 urlBase = os.environ.get('urlBase')
 bearerToken = os.environ.get('bearerToken')
-model = load_model('leaf_model_acc_0_97.h5')
+model = load_model('leaf_model_cnn_accu 0.9279778599739075 --2023-07-15 182517.h5')
 widthImage = 224
 heightImage = 224
 
@@ -43,22 +37,11 @@ def index():
     print('index',flush=True)
     return '<h1> Hola desde flask </h1>'
 
-
-@app.route("/api/model/evalute", methods=["POST"])
-def model_evaluate():
-    # data = request.json
-    # df = pd.DataFrame(data, index=[0])
-    # prediction = model.predict(transformer.transform(df))
-    # predicted_price = expm1(prediction.flatten()[0])
-    # return jsonify({"price": str(predicted_price)})
-    return jsonify({"price":"2324"})
-
 def resize(x):
   return cv2.resize(x, dsize=(widthImage, heightImage), interpolation = cv2.INTER_AREA)
 
 @app.route("/webhook/", methods=["POST", "GET"])
 def webhook_whatsapp():
-    # print('webhook_whatsapp',flush=True)
     if request.method == "GET":
         if request.args.get('hub.verify_token') == "LeafBotModel":
             return request.args.get('hub.challenge')
@@ -66,9 +49,6 @@ def webhook_whatsapp():
           return "Error de autentificacion."
     data=request.get_json()
 
-    # print('data',flush=True)
-    # print(data,flush=True)
-    
     if 'messages' in data['entry'][0]['changes'][0]['value']:
 
         primer_mensaje = data['entry'][0]['changes'][0]['value']['messages'][0]
@@ -86,56 +66,36 @@ def webhook_whatsapp():
             wb = str(urlBase) + str(urlParameter)
 
             print('wb',flush=True)
-            # print(wb,flush=True)
             response = requests.get(wb, headers=headers)
 
             if response.status_code == 200:
                 dataUrl = response.json()
-                print(dataUrl['url'])
-                print(dataUrl['url'])
                 resp = requests.get(dataUrl['url'], headers=headers, stream=True).raw
-
-                print('resp',flush=True)
-                print(resp,flush=True)
 
                 if resp is not None:
                     image = np.asarray(bytearray(resp.read()), dtype="uint8")
                     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
                     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                    print('image',flush=True)
-                    # print(image,flush=True)
-                    print("image.shape")
-                    print(image.shape)
 
                     newImage = resize(image)
-                    print("newImage.shape")
-                    print(newImage.shape)
                     newImage = newImage.reshape(1, 224, 224, 3)
                     
                     predicted_classes = np.argmax(model.predict(newImage), axis=-1)
-                    print('prediction.shape')
-                    print(predicted_classes)    
-                    print(predicted_classes.shape)    
-                    print(type(predicted_classes))    
                     
                     label_to_text =  {1:'Enferma', 0:'Sana'}
                     text_prediction = label_to_text[predicted_classes[0]]
-                    print('text_prediction',flush=True)
-                    print(text_prediction,flush=True)
-                    # cv2.imshow('image',image)
-                    # cv2.waitKey(0)
-                    # with open('image_name.jpg', 'wb') as handler:
-                    #     handler.write(resp.content)
                     enviar(telefonoCliente, "La clasificación es:\n{}".format(text_prediction))
 
                 else:
-                    print('Hubo un error 1',flush=True)
-                    enviar(telefonoCliente, "Hubo un error al obtener la imagen, por favor volve a intentarlo")
+                    enviar(telefonoCliente, 
+                           "Hubo un error al obtener la imagen, por favor volve a intentarlo")
             else:
-                print('Hubo un error 2',flush=True)
-                enviar(telefonoCliente, "Hubo un error al obtener la imagen, por favor volve a intentarlo")
+                enviar(telefonoCliente, 
+                       "Hubo un error al obtener la imagen, por favor volve a intentarlo")
         else:
-            enviar(telefonoCliente, "El sistema solo acepta imagenes de plantas de mango, por favor enviar una imagen a evaluar")
+            enviar(telefonoCliente, 
+                   "El sistema solo acepta imágenes de plantas de mango" +
+                   ", por favor enviar una imagen a evaluar")
     return jsonify({"status": "success"}, 200)
 
 def enviar(telefonoRecibe,respuesta):
